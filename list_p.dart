dc8461667921e89eae2dd44d86ca9645c4c19485
aliiiiiikhan <kali67030@gmail.com>
2024-07-12 14:24:25 +0500

    Commit

---
+++ list_p.dart
@@ -0,0 +1,39 @@
+// import 'dart:async';
+
+// void main() {
+//   List names = ['ahsan ', 'bilal ', 'salim ', 'irfan ', 'ali ', 'baghi'];
+//   List num = [6, 5, 4, 3, 2, 1];
+//   num.sort();
+//   print(num);
+//   num.reversed;
+//   print(num);
+// // sort used for alphabetical order
+//   names.sort();
+//   print(names);
+// // print(names);
+// // print(names.length);
+// // print(names.last);
+// // print(names[names.length-2]);
+// // print(names.reversed);
+// // names[0]='alii';
+// // print(names);
+// // used to update the index by method
+
+// /*
+// names.replaceRange(0, 1, ['bye','hye']);
+// print(names);
+
+// }
+// */
+// // print(names);
+// }
+
+void main(){
+  List studentNames=['bilal','ali','zeshan','asalam','ashsan'];
+  print(studentNames.isEmpty);
+  studentNames.clear();
+  print('f');
+  print(studentNames.isNotEmpty);
+
+
+}
