8f1c9c8772fadbcc22037264d411d32437aa49a6
aliiiiiikhan <kali67030@gmail.com>
2024-07-20 09:42:18 +0500

    Commit

--- lec8.dart
+++ lec8.dart
@@ -1,24 +1,55 @@
-// void main() {
-//   var abc=studentMarkSheet();
-//   print('abc ki value :$abc');
-//   // studentMarkSheet();
-//   // print('===========');
-//   // studentMarkSheet();
+// // void main() {
+// //   var abc=studentMarkSheet();
+// //   print('abc ki value :$abc');
+// //   // studentMarkSheet();
+// //   // print('===========');
+// //   // studentMarkSheet();
+// // }
+
+// // // studentmarksheet is function name
+// // // two types of function void type and return type
+// // studentMarkSheet() {
+// //   String name = 'bilal';
+// //   num subject1 = 76;
+// //   num subject2 = 65;
+// //   num subject3 = 87;
+// //   num obtain_marks = subject3 + subject2 + subject1;
+// //   num percentage = (obtain_marks / 300) * 100;
+// //   if (percentage > 50) {
+// //     print("$name pass hugya");
+// //   } else {
+// //     print('$name fail hugya');
+// //   }
+// //   return name;
+// // }
+// void main(){
+//   studentMArks('ali', 45, 67, 86);
+//   studentMArks('bilal', 75, 67, 86);
+//   studentMArks('ahsan', 45, 57, 86);
+
+// }
+// studentMArks(String name,int subject1,int subject2,int subject3){
+// num obtain_marks=subject1+subject2+subject3;
+// num percentage=(obtain_marks/300)*100;
+// if (percentage>50) {
+
+//   print("$name pass or $percentage");
+
+  
+// }
+// else{
+//   print('$name fail');
+// }
 // }
 
-// // studentmarksheet is function name
-// // two types of function void type and return type
-// studentMarkSheet() {
-//   String name = 'bilal';
-//   num subject1 = 76;
-//   num subject2 = 65;
-//   num subject3 = 87;
-//   num obtain_marks = subject3 + subject2 + subject1;
-//   num percentage = (obtain_marks / 300) * 100;
-//   if (percentage > 50) {
-//     print("$name pass hugya");
-//   } else {
-//     print('$name fail hugya');
-//   }
-//   return name;
-// }
+void main(){
+
+  // simple required
+  studentdata("shaheen", "a", "44");
+
+
+}
+studentdata(String name,String section,String rollnum){
+  print('$name ka rol number: $rollnum & class section $section');
+
+}
---
+++ lec9.dart
@@ -0,0 +1,65 @@
+// void main() {
+//   studentInfo(name: 'ali',city: 'abc');
+// }
+// studentInfo({ required name,required city}){
+//   print('name: $name');
+//   print('city: $city');
+
+// }
+
+
+// arrow function
+
+
+// void  main(){
+//   print(sumOfTwoNumbers(3, 4));
+// }
+// sumOfTwoNumbers(num1 ,num2)=> num1+num2;
+// arrow function used for the same thing in the code as function return
+// sumOfTwoNumbers(num1,num2){
+//   return num1+num2;
+
+// null safety
+// void main(){
+// var a =abc();
+// print('value of a: $a');
+
+// }
+// abc(){
+//   print('hello');
+//   return 'aaa';
+// }
+
+// import 'dart:io';
+
+// void main(){
+// // var abc;
+// // String a=abc;
+// // print(a);
+// studentInfo('ali');
+// }
+// studentInfo({ required
+//  String name}){
+
+  
+// }
+
+// import 'dart:io';
+
+// void main(){
+//   String name= stdin.readLineSync()!;// ! ya null ni huga
+//   // ! exclamanation mark shows that this is compulsory to come the nul string in it
+//   // lazmi kuch aega
+// }
+
+void main(){
+
+}
+studentInfo( {String? name}){
+  // isma string require ha aga chal k isma kuch aega
+  // string name kreaga to error aega
+  // q k name khali ni huga
+  // ya to name only likh de
+  // string? name iska matlab hu b sekhta ha or nai bhi
+
+}
